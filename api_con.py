2f94aeefc2a9c45f6dd7213d21b714c71424be7b
Declan Riddell <drriddell3@gmail.com>
2024-03-08 12:11:26 -0500

    Fixed api_con docstring and removed unused function

--- api_con.py
+++ api_con.py
@@ -5,6 +5,8 @@ Author - Declan Riddell - 2024
 import http.client
 
 class apiCon:
+    """Class for all API info
+    """
 
     headers = {
                         'x-rapidapi-host': "v3.football.api-sports.io",
@@ -12,19 +14,10 @@ class apiCon:
                         }
     
     def get_con():
+        """function to get_con for API
+
+        Returns:
+            conn : returns the http.client connection info
+        """
         conn = http.client.HTTPSConnection("v3.football.api-sports.io")
-
-        headers = {
-                        'x-rapidapi-host': "v3.football.api-sports.io",
-                        'x-rapidapi-key': "c59e06183af6afe95c429e79b0fb0791"
-                        }
-        
         return conn
-    
-    def get_headers():
-
-        headers = {
-                        'x-rapidapi-host': "v3.football.api-sports.io",
-                        'x-rapidapi-key': "c59e06183af6afe95c429e79b0fb0791"
-                        }
-        return headers
